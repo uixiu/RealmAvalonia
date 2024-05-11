2ff4bfde0b6eccc16da6478387609991018d1198
uixiu <uixiu@outlook.com>
2024-05-11 16:38:41 +0100

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MongoDB.Bson;
+using RealmAvalonia;
+using Realms;
+using Realms.Exceptions;
+
+namespace RealmAvalonia
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            string path = @"d:\temp\cats.realm";
+            // Delete the existing realm file
+            try
+            { 
+                                System.IO.File.Delete(path);
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+
+            var config = new RealmConfiguration(@"d:\temp\cats.realm");
+
+
+            _localRealm = Realm.GetInstance(config);
+
+            CatsQuery = _localRealm.All<Cat>(); //.AsQueryable();
+        }
+
+        private Realm _localRealm;
+
+        [ObservableProperty] 
+        private IQueryable<Cat> _catsQuery;
+        
+        public Cat? SelectedCat { get; set; }
+
+        [RelayCommand]
+        private void RemoveCat()
+        {
+            if (SelectedCat == null)
+                return;
+
+            _localRealm.Write(() => { _localRealm.Remove(SelectedCat); });
+        }
+
+        [RelayCommand]
+        public void Populate()
+        {
+            Cat ninja = new() { Name = "Ninja", Age = 1, Breed = "Angora" };
+            Cat nounou = new() { Name = "Nounou", Age = 2, Breed = "Siamese" };
+
+          
+            _localRealm.Write(() => _localRealm.Add(nounou));
+            _localRealm.Write(() => _localRealm.Add(ninja));
+        }
+    }
+}
